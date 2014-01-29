68665885ef2362fcbbe719e5c21e99177b5d9a50
Jonathon Moore <jon@webdigia.com>
2014-01-29 17:48:10 -0500

    initial commit

---
+++ laravel_paste.py
@@ -0,0 +1,20 @@
+from subprocess import call
+import pprint
+import requests
+import sys
+
+file = sys.argv[1]
+
+with open(file, 'r') as content_file: 
+    content = content_file.read()
+
+payload = { 'emp_tee': '', 'paste': content }
+
+paste = requests.post('http://paste.jesse-obrien.ca', data=payload)
+
+if not paste.status_code == requests.codes.ok:
+    sys.exit("Post to paste.jesse-obrien.ca returned a " + paste.status_code)
+
+call(['chrome-open', paste.url])
+
+print "Done! \n" + paste.url
